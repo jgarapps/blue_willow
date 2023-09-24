00a6a016f8eb66dcd4931c0ab60d45db09f7ffb3
Jamie Garcia <48419492+JamieGarcia123@users.noreply.github.com>
2023-09-24 14:49:10 -0500

    config

--- web-template/next.config.js
+++ web-template/next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    mySecret: 'secret',
-    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
-  },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    staticFolder: '/static',
+  output:  process.env.NODE_ENV !== "production" ? undefined: "export",
+  images: {
+    unoptimized: envImageUnoptimize,
   },
 }
 
