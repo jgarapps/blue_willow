f7091428ec923c325921ae9ebc2adcd5fcb1c204
Jamie Garcia <48419492+JamieGarcia123@users.noreply.github.com>
2024-02-04 08:14:21 -0600

    style updates

--- web-template/pages/api/dummydata.tsx
+++ web-template/pages/api/dummydata.tsx
@@ -1,9 +1,10 @@
 import vintageBG from "../../assets/vintage-BG.jpg"
+import image1 from "../../assets/candle_img.webp"
 
 
 export const slugs: any[] = [
   {
-    link: "boudoir",
+    link: "Boudoir",
   },
   {
     link: "glam"
@@ -21,51 +22,105 @@ export const slugData = [
     name: "boudoir",
     details:"",
     more_details:"",
-    image: vintageBG.src
+    image: vintageBG.src,
+    images: [
+      { 
+        image: image1.src,
+        blurb: "some text",
+      },
+      {    
+        image: image1.src,
+        blurb: "",
+      },
+      {    
+        image: image1.src,
+        blurb: ""
+      }
+    ] 
+
+
   },
   {
     name: "glam",
-    population: "1,299,544",
-    date_established: "1856",
-    region: "north texas",
-    classification: "urban",
-    avg_income: "$85,000",
     details:"",
-    isCapital: false,
-    sights: [
-      {
-        name:"",
-        description: ""
-      },
-      {
-        name:"",
-        description: ""
-      },
-      {
-        name:"",
-        description: ""
-      },
-    ],
     more_details:"",
-    image:""
+    image: vintageBG.src,
+    images: [
+      { 
+        image: image1.src,
+        blurb: "",
+      },
+      {    
+        image: image1.src,
+        blurb: "",
+      },
+      {    
+        image: image1.src,
+        blurb: ""
+      }
+    ] 
+   
+  
   },
   {
     name: "about me",
     details:"Hey there! My name is Bobbie and I’ve been doing photography since there was just film. I am a momma to two beautiful girls, a wife to a zany guy, and we have three insane dogs that are always trying to cuddle. My husband and I are both ridiculously big nerds with all things video games, movies, books and more. You’ll often find me in my free time going for nature walks looking for flowers or cool rocks, or even brewing a nice cup of tea or coffee to enjoy while chilling on the patio when it’s nice out. Women’s Empowerment has come to be my favorite category of photography mostly because of a few reasons. It showcases the strength, resilience, and diversity of women. Seeing images of empowered women can inspire and empower others. It can serve as a reminder that women are capable of achieving great things and can break barriers in various fields.",
     more_details:"Above all my favorite part about what I do is often the reveal of the gallery. Showing women just how utterly strikingly beautiful inside and out they really are always gets to me….",
-    image:""
+    image: vintageBG.src,
+    images: [
+      { 
+        image_2: image1.src,
+        blurb: "",
+      },
+      {    
+        image_3: image1.src,
+        blurb: "",
+      },
+      {    
+        image_4: image1.src,
+        blurb: ""
+      }
+    ] 
   },
   {
     name: "galveston",
     details:"A city on an Island",
     more_details:"",
-    image: ""
+    image: vintageBG.src,
+    images: [
+      { 
+        image_2: image1.src,
+        blurb: "",
+      },
+      {    
+        image_3: image1.src,
+        blurb: "",
+      },
+      {    
+        image_4: image1.src,
+        blurb: ""
+      }
+    ] 
   },
   {
     name:"houston",
     details:"",
     more_details:"",
-    image: ""
+    image: vintageBG.src,
+    images: [
+      { 
+        image_2: image1.src,
+        blurb: "",
+      },
+      {    
+        image_3: image1.src,
+        blurb: "",
+      },
+      {    
+        image_4: image1.src,
+        blurb: ""
+      }
+    ] 
   },
 
 ]
